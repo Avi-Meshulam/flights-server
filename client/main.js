86016065ac188687cb44ce0a65119c61a9da2d21
PrisonerM13 <PrisonerM13@outlook.com>
2019-04-09 19:27:11 +0300

    flights-server--client-sort-avi

--- client/main.js
+++ client/main.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const serverURl = 'http://localhost:8080';
+const FlightFields = {
+    id: 'id',
+    from: 'from',
+    to: 'to',
+    departure: 'departure',
+    arrival: 'arrival',
+    airline: 'airline', 
+}
+
 let flightsData = [];
 let tableBody = document.querySelector('#flights-table tbody');
 
@@ -11,18 +20,48 @@ fetch(`${serverURl}/flights.json`)
         renderTable();
     });
 
+document.querySelectorAll('th a').forEach(element => {
+    element.onclick = function () {
+        tableHeadClicked();
+    }
+});
+
+function tableHeadClicked() {
+    const fieldName = event.target.getAttribute('data-field');
+    const sortOrder = event.target.getAttribute('sort-order') === 'asc' ? 'des' : 'asc';
+
+    switch (fieldName) {
+        case FlightFields.departure:
+        case FlightFields.arrival:
+            flightsData.sort(getSortFunc(sortOrder, (flight) => new Date(flight[fieldName])));
+            break;
+        default:
+            flightsData.sort(getSortFunc(sortOrder, (flight) => flight[fieldName]));
+            break;
+    }
+
+    event.target.setAttribute('sort-order', sortOrder);
+    renderTable();
+}
+
+function getSortFunc(order = 'asc', itemFunc = (x) => x) {
+    return order === 'asc' ? 
+        (a, b) => itemFunc(a) >= itemFunc(b) ? 1 : -1 :
+        (a, b) => itemFunc(a) <= itemFunc(b) ? 1 : -1;
+}
+
 function renderTable() {
     let tbody = document.createElement('tbody');
 
     flightsData.forEach(entry => {
         const tr = document.createElement('tr');
         tr.innerHTML = `
-            <td>${entry.id}</td>
-            <td>${entry.from}</td>
-            <td>${entry.to}</td>
-            <td>${entry.departure}</td>
-            <td>${entry.arrival}</td>
-            <td>${entry.by}</td>
+            <td>${entry[FlightFields.id]}</td>
+            <td>${entry[FlightFields.from]}</td>
+            <td>${entry[FlightFields.to]}</td>
+            <td>${entry[FlightFields.departure]}</td>
+            <td>${entry[FlightFields.arrival]}</td>
+            <td>${entry[FlightFields.airline]}</td>
         `;
         tbody.appendChild(tr)
     });
